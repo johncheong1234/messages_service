5bb24773a8f1b366cf668363d3456b6bfa7bf662
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-18 22:35:35 +0800

    change to select where OR

--- app/controllers/message.controller.js
+++ app/controllers/message.controller.js
@@ -88,7 +88,7 @@ exports.findAllByName = (req,res)=>{
   var sender = req.params.name;
   var receiver = req.params.name;
   console.log(req.params.name)
-  messages.findAll({where: {sender: req.params.name, receiver: req.params.name}})
+  messages.findAll({where: $or[{sender: req.params.name}, {receiver: req.params.name}]})
   .then(data => {
     res.send(data);
   })
